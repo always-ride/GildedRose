bedf2c43ced11ac7cc73af4fcc5ca19209b573b5
Daniel Fahrni <daniel.fahrni@junamin.ch>
2025-07-04 15:46:36 +0200

    refactoring of unit test, renaming, etc.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,13 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-
+    
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void testItemNameDoesNotChange() {
+        Item item = new Item("foo", 0, 0);
+        GildedRose app = new GildedRose(new Item[] { item });
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals("foo", item.name);
     }
-
 }
